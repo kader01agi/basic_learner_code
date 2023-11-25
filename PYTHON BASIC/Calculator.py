1822828fd6ee6c5b63b35188967c09e6a65ebe4f
kader01agi <md.abdul.kader.ru@gmail.com>
2023-11-25 13:16:16 +0600

    updated

--- PYTHON BASIC/Calculator.py
+++ PYTHON BASIC/Calculator.py
@@ -1,7 +1,7 @@
 # Unpopular mundane method
-x = input("What is x? ")
-y = input("What is y? ")
-z = int(x) + int(y)
+x = int(input("What is x? "))
+y = int(input("What is y? "))
+z = x + y
 print(z)
 
 # TALENTED METHOD
@@ -30,45 +30,45 @@ print(f"{z:,}")
 
 
 
-# Rounds decimal to 2 digit.
-z = round(x / y, 2)
-print(z)
+# # Rounds decimal to 2 digit.
+# z = round(x / y, 2)
+# print(z)
 
-# GRADE Calculator Mundane/Boring
-score = int(input("Score: "))
-if score >= 90 and score <= 100:
-    print ("Grade: A")
-elif score >= 80 and score < 90:
-    print("Grade: B")
-elif score >= 70 and score < 80:
-    print("Grade: C")
-elif score >= 60 and score < 70:
-    print ("Grade: D")
-else:
-    print ("Grade: F")
+# # GRADE Calculator Mundane/Boring
+# score = int(input("Score: "))
+# if score >= 90 and score <= 100:
+#     print ("Grade: A")
+# elif score >= 80 and score < 90:
+#     print("Grade: B")
+# elif score >= 70 and score < 80:
+#     print("Grade: C")
+# elif score >= 60 and score < 70:
+#     print ("Grade: D")
+# else:
+#     print ("Grade: F")
 
-# GRADE Calculator Dull/Boring
-score = int(input("Score: "))
-if 90 <= score <= 100:
-    print ("Grade: A")
-elif 80<= score < 90:
-    print("Grade: B")
-elif 70 <= score < 80:
-    print("Grade: C")
-elif 60 <= score < 70:
-    print ("Grade: D")
-else:
-    print ("Grade: F")
+# # GRADE Calculator Dull/Boring
+# score = int(input("Score: "))
+# if 90 <= score <= 100:
+#     print ("Grade: A")
+# elif 80<= score < 90:
+#     print("Grade: B")
+# elif 70 <= score < 80:
+#     print("Grade: C")
+# elif 60 <= score < 70:
+#     print ("Grade: D")
+# else:
+#     print ("Grade: F")
 
-# GRADE Calculator TALLENTED
-score = int(input("Score: "))
-if score >= 90:
-    print ("Grade: A")
-elif score >= 80:
-    print("Grade: B")
-elif score >= 70:
-    print ("Grade: C")
-elif score >= 60:
-    print ("Grade: D")
-else:
-    print ("Grade: F")
+# # GRADE Calculator TALLENTED
+# score = int(input("Score: "))
+# if score >= 90:
+#     print ("Grade: A")
+# elif score >= 80:
+#     print("Grade: B")
+# elif score >= 70:
+#     print ("Grade: C")
+# elif score >= 60:
+#     print ("Grade: D")
+# else:
+#     print ("Grade: F")
