fbc8fc5997bbc325a9c03001d9706494fab4e19f
kader01agi <md.abdul.kader.ru@gmail.com>
2023-12-05 10:25:54 +0600

    subtle

--- dev.py
+++ dev.py
@@ -1,136 +1,138 @@
-# input1 = int(input("what is input1? "))
+# # input1 = int(input("what is input1? "))
 
-# # output 1
-# output1 = input1 % 10
-# print(output1)
-
-
-# # output 2
-# if input1 % 2 == 0:
-#     print("Even")
-# else:
-#     print("Odd")
-
-# # output 3
-# output3 = []
-# if input1 % 2 == 0:
-#     output3.append(input1)
-#     print(output3)
-# else:
-#     print("Odd")
-
-# # output 4
+# # # output 1
+# # output1 = input1 % 10
+# # print(output1)
 
 
+# # # output 2
+# # if input1 % 2 == 0:
+# #     print("Even")
+# # else:
+# #     print("Odd")
 
-# sample = [2, 4, 4, 4, 5, 5, 7, 9]
-# def my_sqrt(x):
-#   if x < 0:
-#     raise ValueError("Cannot calculate square root of a negative number")
-#   elif x == 0:
-#     return 0
-#   else:
-#     guess = 1
-#     while guess * guess <= x:
-#       guess += 1
-#     return guess - 1
+# # # output 3
+# # output3 = []
+# # if input1 % 2 == 0:
+# #     output3.append(input1)
+# #     print(output3)
+# # else:
+# #     print("Odd")
+
+# # # output 4
+
+
+
+# # sample = [2, 4, 4, 4, 5, 5, 7, 9]
+# # def my_sqrt(x):
+# #   if x < 0:
+# #     raise ValueError("Cannot calculate square root of a negative number")
+# #   elif x == 0:
+# #     return 0
+# #   else:
+# #     guess = 1
+# #     while guess * guess <= x:
+# #       guess += 1
+# #     return guess - 1
   
 
-def my_sqrt(e):
-#   if x < 0:
-#     raise ValueError("Cannot calculate square root of a negative number")
-#   elif x == 0:
-#     return 0
-#   else:
-    num = 1
-    while num * num <= e:
-        num += 1
-    return num - 1
+# def my_sqrt(e):
+# #   if x < 0:
+# #     raise ValueError("Cannot calculate square root of a negative number")
+# #   elif x == 0:
+# #     return 0
+# #   else:
+#     num = 1
+#     while num * num <= e:
+#         num += 1
+#     return num - 1
 
 
-# Code for getting output 1.
-input1 = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
-two_d_list = []
+# # Code for getting output 1.
+# input1 = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+# two_d_list = []
 
-a= []
-for i in input1[0]:
-    x = i % 10
-    a.append(x)
-two_d_list.append(a)
+# a= []
+# for i in input1[0]:
+#     x = i % 10
+#     a.append(x)
+# two_d_list.append(a)
 
-b = []
-for j in input1[1]:
-    y = j % 10
-    b.append(y)
-two_d_list.append(b) 
+# b = []
+# for j in input1[1]:
+#     y = j % 10
+#     b.append(y)
+# two_d_list.append(b) 
 
-c = []
-for k in input1[2]:
-    z = k % 10
-    c.append(z)  
-two_d_list.append(c)
+# c = []
+# for k in input1[2]:
+#     z = k % 10
+#     c.append(z)  
+# two_d_list.append(c)
 
-d = []
-for l in input1[3]:
-    m = l % 10
-    d.append(m)  
-two_d_list.append(d)
-
-
-print(two_d_list)
+# d = []
+# for l in input1[3]:
+#     m = l % 10
+#     d.append(m)  
+# two_d_list.append(d)
 
 
-
-# Code for getting output 2.
-odd_even_two_d_list = []
-
-oe1 = []
-for i in input1[0]:
-    if i % 2 == 0:
-        oe1.append("Even")
-    else:
-        oe1.append("Odd")
-odd_even_two_d_list.append(oe1)
-
-oe2 = []
-for i in input1[0]:
-    if i % 2 == 0:
-        oe2.append("Even")
-    else:
-        oe2.append("Odd")
-odd_even_two_d_list.append(oe2)
-
-oe3 = []
-for i in input1[0]:
-    if i % 2 == 0:
-        oe3.append("Even")
-    else:
-        oe3.append("Odd")
-odd_even_two_d_list.append(oe3)
-
-oe4 = []
-for i in input1[0]:
-    if i % 4 == 0:
-        oe4.append("Even")
-    else:
-        oe4.append("Odd")
-odd_even_two_d_list.append(oe4)
-
-print(odd_even_two_d_list)
+# print(two_d_list)
 
 
-# Code for getting output 3.
-output3_list = []
-for i in two_d_list:
-    sum = 0
-    for j in i:
-        sum += j
-    output3_list.append(sum)    
-print(output3_list)
 
-# Code for getting output 4.
-output4_list = []
-for i in output3_list:
-    if i % 2 == 0:
-        output4_list.append("Even")
-print(output4_list)
+# # Code for getting output 2.
+# odd_even_two_d_list = []
+
+# oe1 = []
+# for i in input1[0]:
+#     if i % 2 == 0:
+#         oe1.append("Even")
+#     else:
+#         oe1.append("Odd")
+# odd_even_two_d_list.append(oe1)
+
+# oe2 = []
+# for i in input1[0]:
+#     if i % 2 == 0:
+#         oe2.append("Even")
+#     else:
+#         oe2.append("Odd")
+# odd_even_two_d_list.append(oe2)
+
+# oe3 = []
+# for i in input1[0]:
+#     if i % 2 == 0:
+#         oe3.append("Even")
+#     else:
+#         oe3.append("Odd")
+# odd_even_two_d_list.append(oe3)
+
+# oe4 = []
+# for i in input1[0]:
+#     if i % 4 == 0:
+#         oe4.append("Even")
+#     else:
+#         oe4.append("Odd")
+# odd_even_two_d_list.append(oe4)
+
+# print(odd_even_two_d_list)
+
+
+# # Code for getting output 3.
+# output3_list = []
+# for i in two_d_list:
+#     sum = 0
+#     for j in i:
+#         sum += j
+#     output3_list.append(sum)    
+# print(output3_list)
+
+# # Code for getting output 4.
+# output4_list = []
+# for i in output3_list:
+#     if i % 2 == 0:
+#         output4_list.append("Even")
+# print(output4_list)
+
+
